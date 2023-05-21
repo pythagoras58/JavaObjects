48d1ba1ed0dae371fdc40f83084e32505df373cb
pythagoras58 <solomondanso58@gmail.com>
2023-05-22 00:51:19 +0800

    Main Class

--- src/main/java/inheritance/EmpClass.java
+++ src/main/java/inheritance/EmpClass.java
@@ -1,4 +1,43 @@
 package inheritance;
 
 public class EmpClass {
+
+    private int empId;
+
+    private String name;
+
+    private String empDate;
+
+    public EmpClass() {
+    }
+
+    public EmpClass(int empId, String name, String empDate) {
+        this.empId = empId;
+        this.name = name;
+        this.empDate = empDate;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmpDate() {
+        return empDate;
+    }
+
+    public void setEmpDate(String empDate) {
+        this.empDate = empDate;
+    }
 }
---
+++ src/main/java/inheritance/MainClass.java
@@ -0,0 +1,7 @@
+package inheritance;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+}
