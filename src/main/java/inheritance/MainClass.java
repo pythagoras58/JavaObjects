943d41f458b45a43782660911a13eff45b593ef3
pythagoras58 <solomondanso58@gmail.com>
2023-05-22 00:55:52 +0800

    Main Class final

--- src/main/java/inheritance/MainClass.java
+++ src/main/java/inheritance/MainClass.java
@@ -3,5 +3,15 @@ package inheritance;
 public class MainClass {
     public static void main(String[] args) {
 
+        EmpClass empClass = new EmpClass();
+
+        empClass.setEmpId(12);
+        empClass.setName("Pythagoras web");
+        empClass.setEmpDate("2023-05-22");
+
+        System.out.println("=====================EMP DATA============");
+        System.out.println("EMP ID =====> " + empClass.getEmpId());
+        System.out.println("EMP NAME====> " + empClass.getName());
+        System.out.println("EMP DATA====> " + empClass.getEmpDate());
     }
 }
